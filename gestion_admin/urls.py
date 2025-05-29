f4ed85364c5951931fc9744250e2a72935fc0dcd
binobino240982 <allechiobin@gmail.com>
2025-05-29 13:04:58 +0000

    Initial commit for Heroku deployment

--- gestion_admin/urls.py
+++ gestion_admin/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard_admin'),
+    path('', views.dashboard, name='admin_dashboard'),
+    path('accueil/', views.accueil_dashboard, name='accueil_dashboard'),
+    path('dashboard-detail/', views.dashboard_admin, name='dashboard_admin'),
+    path('candidatures/', views.liste_candidatures, name='liste_candidatures'),
 ]
